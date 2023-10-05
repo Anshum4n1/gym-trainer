48ba5981211c09768c9302accedb80e38c5d51c7
Anshuman Rathore <anshumansr.cs.20@nitj.ac.in>
2023-10-05 14:59:32 +0530

    added bootstrap css

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Calendar from './pages/Calendar';
+import Header from './components/Header';
 function App() {
 
   return (
-    <>
+    <div>
     <Router>
+    <Header />
     <Routes>
        <Route path="/" element={<Dashboard />} />
         <Route path="/calendar" element={<Calendar/>}/>
-
       </Routes>
       </Router>
       
-    </>
+    </div>
   )
 }
 
